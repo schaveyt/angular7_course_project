441745f91c8adea2915472577f2a6d98a845f576
Todd Schavey <schaveyt@gmail.com>
2018-11-11 00:25:08 -0500

    looping thru recipes and displaying as cards

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Array<Recipe> = [];
 
-  constructor() { }
+  constructor() {
+    this.recipes.push(new Recipe('A Test recipe', 'Some Description',
+      'https://cdn.pixabay.com/photo/2018/10/24/00/43/herb-3769381_960_720.jpg'));
+      this.recipes.push(new Recipe('A Test recipe', 'Some Description',
+      'https://cdn.pixabay.com/photo/2018/10/24/00/43/herb-3769381_960_720.jpg'));
+      this.recipes.push(new Recipe('A Test recipe', 'Some Description',
+      'https://cdn.pixabay.com/photo/2018/10/24/00/43/herb-3769381_960_720.jpg'));
+      this.recipes.push(new Recipe('A Test recipe', 'Some Description',
+      'https://cdn.pixabay.com/photo/2018/10/24/00/43/herb-3769381_960_720.jpg'));
+  }
 
   ngOnInit() {
   }
