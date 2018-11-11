40b3581dc8371ec75fc5d5c322e44c4676027035
Todd Schavey <schaveyt@gmail.com>
2018-11-10 22:28:22 -0500

    initial components skeleton

---
+++ src/app/shopping-list/shopping-list-editor/shopping-list-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list-editor',
+  templateUrl: './shopping-list-editor.component.html',
+  styleUrls: ['./shopping-list-editor.component.css']
+})
+export class ShoppingListEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
