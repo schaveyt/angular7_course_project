999d733f9f41f6f8988df00638fddf01d0b66f1a
Todd Schavey <schaveyt@gmail.com>
2018-11-20 11:36:59 -0500

    implemented hack on the shopping list form per instructions

--- src/app/components/shopping-list-editor/shopping-list-editor.component.ts
+++ src/app/components/shopping-list-editor/shopping-list-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-editor',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-editor.component.css']
 })
 export class ShoppingListEditorComponent implements OnInit {
+  @ViewChild('inputName') inputName: ElementRef;
+  @ViewChild('inputAmount') inputAmount: ElementRef;
+  @Output() addIngredint = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAdd() {
+    const name = this.inputName.nativeElement.value;
+    const amount = this.inputAmount.nativeElement.value;
+    const ingredint = new Ingredient(name, amount);
+    this.addIngredint.emit(ingredint);
   }
 
 }
--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -16,4 +16,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onAddIngredient(i: Ingredient) {
+    this.ingredients.push(i);
+  }
+
 }
